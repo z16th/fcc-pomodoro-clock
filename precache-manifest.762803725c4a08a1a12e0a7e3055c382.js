1e32d790c58800db7b5654a6b4fde6b403dafef2
LoLz XVI <47308963+LoLzWTFomfg@users.noreply.github.com>
2020-06-19 07:45:11 -0500

    Updates

---
+++ precache-manifest.762803725c4a08a1a12e0a7e3055c382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348d3d15d74dd4af3f5a43be2984ff15",
+    "url": "/fcc-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "9794d54459d18bea73b5",
+    "url": "/fcc-pomodoro-clock/static/css/main.28e45389.chunk.css"
+  },
+  {
+    "revision": "0eea6f80555a8e1986b5",
+    "url": "/fcc-pomodoro-clock/static/js/2.a215b2a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc-pomodoro-clock/static/js/2.a215b2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9794d54459d18bea73b5",
+    "url": "/fcc-pomodoro-clock/static/js/main.566235da.chunk.js"
+  },
+  {
+    "revision": "c4d838c95c6b2442d3ff",
+    "url": "/fcc-pomodoro-clock/static/js/runtime-main.90e9e5dd.js"
+  }
+]);
